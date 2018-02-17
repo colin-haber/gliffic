3149146362ed260d290d90be70ac9792c2619c54
Colin Haber <colin@haber.sexy>
2018-02-17 09:29:51 -0500

    Add progress and status display.

--- scripts/gliffic.js
+++ scripts/gliffic.js
@@ -20,7 +20,7 @@ var Gliffic = Gliffic || {
 				set = Gliffic.codepoints.filter(codepoint => query.split(/\s+/).every(term => codepoint.name.toUpperCase().includes(term)));
 			}
 		} else { // Default to single-byte characters
-			set = Gliffic.codepoints.filter(codepoint => codepoint.index < 256);
+			set = Gliffic.defaultSet || (Gliffic.defaultSet = Gliffic.codepoints.filter(codepoint => codepoint.index < 256));
 		}
 		set.sort((a, b) => a.index - b.index);
 		template.parentElement.querySelectorAll(".glyph").forEach(element => element.remove());
@@ -32,11 +32,30 @@ var Gliffic = Gliffic || {
 			template.parentElement.appendChild(document.importNode(frag, true));
 		});
 	},
+	defaultSet: null,
 	unicodeDataPath: "data/UnicodeData-11.0.0d11.txt",
 	unicodeVersion: "11.0.0d11"
 }
 document.addEventListener("DOMContentLoaded", async function (DOMContentLoaded) {
+	let progress = document.querySelector(".control-progress");
+	let status = document.querySelector(".control-status");
+	progress.max = 4;
+	progress.value = 0;
+	progress.dataset.targetValue = 0;
 	let params = new URL(window.location).searchParams;
+	let updateProgressBar = function () {
+		if (progress.value < progress.dataset.targetValue) {
+			progress.value += Math.max((progress.dataset.targetValue - progress.value) / 12, 0.001);
+		}
+		if (progress.value < progress.max) {
+			requestAnimationFrame(updateProgressBar);
+		} else {
+			progress.value = progress.max;
+			progress.removeAttribute("data-target-value");
+		}
+	};
+	updateProgressBar();
+	status.textContent = "Adding listeners\u2026";
 	if (params.has("search")) {
 		document.querySelector(".control-search").value = params.get("search");
 	}
@@ -62,13 +81,16 @@ document.addEventListener("DOMContentLoaded", async function (DOMContentLoaded) 
 		document.title = query ? `${query} | Gliffic` : "Gliffic";
 		Gliffic.createAndDisplayTable(query);
 	});
+	progress.dataset.targetValue++;
 	if (window.localStorage.getItem("unicodeVersion") !== Gliffic.unicodeVersion) {
 		indexedDB.deleteDatabase("unicode");
+		status.textContent = "Downloading Unicode data\u2026";
 		let codepoints = (await (await window.fetch(Gliffic.unicodeDataPath)).text()).split(/[\r\n]+/).filter(line => line).map(line => {
 			let split = line.split(";");
 			let index = parseInt(split[0], 16);
 			return new Gliffic.Codepoint(index, String.fromCodePoint(index), ...split);
 		});
+		status.textContent = "Storing Unicode data\u2026";
 		indexedDB.open("unicode").addEventListener("upgradeneeded", function (upgradeneeded) {
 			let unicodeDb = upgradeneeded.target.result;
 			let codepointStore = unicodeDb.createObjectStore("codepoints", {
@@ -81,6 +103,8 @@ document.addEventListener("DOMContentLoaded", async function (DOMContentLoaded) 
 		});
 		window.localStorage.setItem("unicodeVersion", Gliffic.unicodeVersion);
 	}
+	status.textContent = "Loading Unicode data\u2026";
+	progress.dataset.targetValue++;
 	indexedDB.open("unicode").addEventListener("success", function (success) {
 		let unicodeDb = success.target.result;
 		let codepointStore = unicodeDb.transaction("codepoints").objectStore("codepoints");
@@ -90,6 +114,7 @@ document.addEventListener("DOMContentLoaded", async function (DOMContentLoaded) 
 				Gliffic.codepoints.push(cursor.value);
 				cursor.continue();
 			} else {
+				progress.dataset.targetValue++;
 				if (params.has("search")) {
 					let query = params.get("search").trim();
 					document.title = `${query} | Gliffic`
@@ -97,6 +122,8 @@ document.addEventListener("DOMContentLoaded", async function (DOMContentLoaded) 
 				} else {
 					Gliffic.createAndDisplayTable();
 				}
+				progress.dataset.targetValue++;
+				status.textContent = "";
 			}
 		});
 	});
